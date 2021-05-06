1cfacc9ee1e4319ab5c4ef9ba309b7542fb30c69
Brandon Ling <brandonvling@gmail.com>
2021-05-06 18:46:26 -0400

    Add initial welcome page

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get welcome" do
+    get welcome_url
+    assert_response :success
+  end
+end
