8e7be6187bcf87ef468707e8fb8b10acac90ca8f
Brandon Ling <brandonvling@gmail.com>
2021-05-06 20:36:19 -0400

    Validate User name and email presence

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "foobar@example.com")
+  end
+
+  test "user should have a name" do
+    assert @user.valid?
+    @user.name = "    "
+    assert_not @user.valid?
+  end
+
+  test "user should have an email" do
+    assert @user.valid?
+    @user.email = "   " 
+    assert_not @user.valid?
+  end
 end
