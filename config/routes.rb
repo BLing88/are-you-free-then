18f22df264567f1963940d8d3a54e6f26cdff635
Brandon Ling <brandonvling@gmail.com>
2021-05-08 11:02:18 -0400

    Use /signup path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#welcome'
   get '/welcome', to: 'static_pages#welcome'
   get '/signup', to: 'users#new'
-  resources :users, except: :index
+  post '/signup', to: 'users#create'
+  resources :users, except: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
