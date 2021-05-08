acccd6bf7c0710241f963a31118667d45f8a3bd2
Brandon Ling <brandonvling@gmail.com>
2021-05-07 21:57:58 -0400

    Add Users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#welcome'
   get '/welcome', to: 'static_pages#welcome'
+  resources :users, except: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
