5b71fb508c1fa6f73b74178314f0eb961ae86750
Brandon Ling <brandonvling@gmail.com>
2021-05-08 14:58:34 -0400

    Add initial log in page

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,8 +1,17 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
-    assert_response :success
+  test "login page" do
+    get login_path
+    assert_select "h1", /log in/i
+    assert_select "label", /email/i
+    assert_select "input[id=?]", "session_email"
+    assert_select "label", /password/i
+    assert_select "input[id=?]", "session_password"
+    assert_select "a[href=?]", signup_url
+  end
+
+  test "log in with invalid information" do
+    get login_path
   end
 end
