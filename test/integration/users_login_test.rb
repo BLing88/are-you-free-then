3ddad9e8de2a553e5cc28d6834decd2ec08c7946
Brandon Ling <brandonvling@gmail.com>
2021-05-08 15:55:08 -0400

    Add Sessions helpers

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:alice)
+  end
+
   test "login path" do
     get login_path
     assert_select "h1", /log in/i
@@ -19,5 +23,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "log in with valid information" do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'example_password' } } 
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
