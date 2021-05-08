a40c2ff556a5a8f80a012d41795da149c860e24f
Brandon Ling <brandonvling@gmail.com>
2021-05-08 00:59:11 -0400

    Add sign up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#welcome'
   get '/welcome', to: 'static_pages#welcome'
+  get '/signup', to: 'users#new'
   resources :users, except: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
