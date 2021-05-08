5bcb74102dd86e12b0926ca0ead8845cdb365277
Brandon Ling <brandonvling@gmail.com>
2021-05-08 14:06:41 -0400

    Generate Sessions controller and login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#welcome'
   get '/welcome', to: 'static_pages#welcome'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#delete'
   resources :users, except: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
