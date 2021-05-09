acc710c5b9ed24e8639b6d4104504e1a4105c887
Brandon Ling <brandonvling@gmail.com>
2021-05-09 13:45:35 -0400

    Fix input name typo

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,19 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "signup page" do
+    get signup_path
+    assert_select 'input[name=?]', "user[name]"
+    assert_select 'label', /name/i
+    assert_select 'input[name=?]', "user[email]"
+    assert_select 'label', /email/i
+    assert_select 'input[name=?]', "user[password]"
+    assert_select 'label', /password/i
+    assert_select 'label', /password confirmation/i
+    assert_select 'input[name=?]', "user[password_confirmation]"
+    assert_select 'input[type="submit"]'
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
