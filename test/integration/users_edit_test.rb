96f15941c1b57ea7e56b1b505739b3c538ec176b
Brandon Ling <brandonvling@gmail.com>
2021-05-09 01:06:46 -0400

    Require logged in and correct user to edit and update their own profile

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,8 +3,37 @@ require "test_helper"
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:alice)
+    @other_user = users(:bob)
   end
 
+  test 'should redirect edit when not logged in' do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect update when not logged in' do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect edit when logged in as wrong user' do
+    log_in_as(@other_user)
+    get edit_user_path @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect update when logged in as wrong user' do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+  
   test 'unsuccessful edit' do
     log_in_as(@user)
     get edit_user_path(@user)
