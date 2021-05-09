4bc9f56d49d0c33aa120bcbaba6575e1b654402c
Brandon Ling <brandonvling@gmail.com>
2021-05-09 00:35:59 -0400

    Handle successful User updates

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test 'unsuccessful edit' do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_select 'h1', /update your profile/i
     patch user_path(@user), params: { user: { name: "",
@@ -14,5 +15,45 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                              password_confirmation: 'bar' } }
     assert_select 'h1', /update your profile/i
     assert_select 'div', /form contains 4 errors/i
+  end
+
+  test "successful edit including password" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_select 'h1', /update your profile/i
+    name = "Foo Bar"
+    email = "alice@foo.com"
+    password = "new_password"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: password,
+                                              password_confirmation: password } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select 'div', /profile updated successfully/i
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+    assert BCrypt::Password.new(@user.password_digest).is_password?(password)
+  end
+  
+  test "successful edit without password" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_select 'h1', /update your profile/i
+    name = "Dog Cat"
+    email = "bob@foo.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_select 'div', /profile updated successfully/i
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
   end
 end
