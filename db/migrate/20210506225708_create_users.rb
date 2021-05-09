c16ea7f14aafcfa8ac3dadc1c55b3eb6455bf030
Brandon Ling <brandonvling@gmail.com>
2021-05-09 18:43:51 -0400

    Switch to Postgresql db for development and tests

--- db/migrate/20210506225708_create_users.rb
+++ db/migrate/20210506225708_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
+      t.text :name
+      t.text :email
 
       t.timestamps
     end
--- db/migrate/20210508235658_add_remember_digest_to_users.rb
+++ db/migrate/20210508235658_add_remember_digest_to_users.rb
@@ -1,5 +1,5 @@
 class AddRememberDigestToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :remember_digest, :string
+    add_column :users, :remember_digest, :text
   end
 end
