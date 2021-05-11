af5a64236d4c7978d501d016a221f32c66e59d65
Brandon Ling <brandonvling@gmail.com>
2021-05-10 22:00:07 -0400

    Move where CalendarInput is being loaded

--- app/javascript/packs/CalendarInput.tsx
+++ app/javascript/packs/CalendarInput.tsx
@@ -3,5 +3,8 @@ import { render } from "react-dom";
 import { Calendar } from "./components/Calendar";
 
 document.addEventListener("turbolinks:load", () => {
-  render(<Calendar />, document.getElementById("react-calendar-input"));
+  const container = document.getElementById("react-calendar-input");
+  if (container) {
+    render(<Calendar />, container);
+  }
 });
