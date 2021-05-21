a1ca297c92d772d17991fb2c625d88a74449059e
Brandon Ling <brandonvling@gmail.com>
2021-05-20 22:03:19 -0400

    Remove associated free times when deleting a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
 
-  has_many :free_times
+  has_many :free_times, dependent: :destroy
   has_many :time_intervals, through: :free_times
 
   # Returns the hash digest of the given string
--- test/integration/users_create_free_times_test.rb
+++ test/integration/users_create_free_times_test.rb
@@ -17,12 +17,35 @@ class UsersCreateFreeTimesTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
-  
+
   test "can create free times" do
     log_in_as(@user)
     assert_difference ['FreeTime.count', 'TimeInterval.count'], 2 do
       post free_times_path, params: {
         create_intervals: ["2021-05-17T18:30:00.000Z_2021-05-17T19:30:00.000Z", "2021-05-17T20:15:00.000Z_2021-05-17T21:15:00.000Z"] }
     end
+  end
+
+  test 'can delete free times' do
+    log_in_as(@user)
+    post free_times_path, params: {
+      create_intervals: ["2021-05-17T18:30:00.000Z_2021-05-17T19:30:00.000Z", "2021-05-17T20:15:00.000Z_2021-05-17T21:15:00.000Z"] }
+    assert_difference 'FreeTime.count', -1 do
+      post free_times_path, params: {
+        delete_intervals: ["2021-05-17T18:30:00.000Z_2021-05-17T19:30:00.000Z"] }
+    end
+  end
+
+  test 'can update free times' do
+    log_in_as(@user)
+    start_time = "2021-05-06T09:15:00.000Z"
+    end_time = "2021-05-06T10:15:00.000Z"
+    assert_no_difference 'FreeTime.count' do
+      post free_times_path, params: {
+        delete_intervals: ["#{free_times(:one).time_interval.start_time}_#{free_times(:one).time_interval.end_time}"],
+        create_intervals: ["#{start_time}_#{end_time}"] }
+    end
+    assert_not @user.reload.free_times.include?(free_times(:one))
+    assert_not @user.time_intervals.where(["start_time = ? AND end_time = ?", start_time, end_time]).nil?
   end
 end
