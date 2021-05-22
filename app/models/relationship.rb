4614cfabf7ee4aa92037109554677e2367f4a67c
Brandon Ling <brandonvling@gmail.com>
2021-05-22 00:46:56 -0400

    Validate Relationships

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,8 +1,17 @@
+class RequestorRequestedNotSameValidator < ActiveModel::Validator
+  def validate(relationship)
+    unless relationship.requestor != relationship.requested || relationship.requestor == nil && relationship.requested == nil
+      relationship.errors.add :users, "Requested user cannot be the same as the requesting user."
+    end
+  end
+end
+
 class Relationship < ApplicationRecord
   after_create :create_pending_status
   has_one :relationship_status, dependent: :destroy
   belongs_to :requestor, class_name: "User"
   belongs_to :requested, class_name: "User"
+  validates_with RequestorRequestedNotSameValidator
 
   private 
     
--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:alice)
+  end
+
+  test 'requestor and requested are required' do
+    rel = Relationship.new(requestor_id: nil, requested_id: nil)
+    assert_not rel.valid?
+    assert_equal 2, rel.errors.full_messages.count
+  end
+
+  test 'creates a pending RelationshipStatus after creating a new Relationship' do
+    rel = Relationship.new(requestor: users(:bob), requested: users(:eve))
+    assert_difference ['Relationship.count', 'RelationshipStatus.count'], 1 do
+        rel.save
+      end
+    assert_equal 'Pending', rel.relationship_status.name
+  end
+
+  test 'requestor cannot be same as requested user' do
+    rel = Relationship.new requestor: @user, requested: @user
+    assert_not rel.valid?
+    assert_equal 1, rel.errors.full_messages.count
+  end
+
+  test 'deleting a relationship deletes associated relationship status' do
+    rel = @user.relationships.first
+    status = rel.relationship_status
+    assert_not_nil RelationshipStatus.find(status.id)
+    rel.destroy
+    assert_nil RelationshipStatus.find_by(id: status.id)
+  end
 end
