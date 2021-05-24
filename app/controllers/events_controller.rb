b73a7f757bb75f4a511950c61abd66a5c098fa36
Brandon Ling <brandonvling@gmail.com>
2021-05-24 17:34:32 -0400

    Create create event page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,22 @@
 class EventsController < ApplicationController
   before_action :logged_in_user
   def new
+    @hosting_event = current_user.hosting_events.build
   end
+
+  def create
+    @hosting_event = current_user.hosting_events.build(event_params)
+    if @hosting_event.save
+      flash[:success] = "Event created!"
+      redirect_to @hosting_event
+    else
+      render 'new'
+    end
+  end
+
+  private
+    
+    def event_params
+      params.require(:event).permit(:host_id, :name)
+    end
 end
--- test/integration/users_create_events_test.rb
+++ test/integration/users_create_events_test.rb
@@ -18,5 +18,24 @@ class UsersCreateEventsTest < ActionDispatch::IntegrationTest
     get new_event_path
     assert_select "title", text: /Create event/i
     assert_select "h1", text: /create a new event/i
+    assert_select "input[name=?]", 'event[name]'
+    assert_select 'label', /name/i
+    assert_select "input[name=?]", 'event[host_id]'
+  end
+
+  test "blank name shows error" do
+    log_in_as @alice
+    get new_event_path
+    post events_path, params: { event: { name: "" } }
+    assert_select 'li', text: /name can't be blank/i
+  end
+  
+  test "successful event creation" do
+    log_in_as @alice
+    event_name = "Another event"
+    assert_difference 'Event.count', 1 do
+      post events_path, params: { event: { name: event_name } }
+    end
+    assert_redirected_to Event.find_by(name: event_name, host: @alice)
   end
 end
