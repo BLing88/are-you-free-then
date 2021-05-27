2350519e57e07e8e5daaf31745865e9de4913dfa
Brandon Ling <brandonvling@gmail.com>
2021-05-27 18:27:01 -0400

    Accept and delete event invites

---
+++ app/controllers/participations_controller.rb
@@ -0,0 +1,31 @@
+class ParticipationsController < ApplicationController
+  before_action :logged_in_user, :correct_user, :correct_event_invite
+  def create
+    begin
+      Participation.transaction do
+        Participation.create!(participation_params)
+        EventInvite.find_by(event_id: params[:event_id], invitee_id: params[:user_id]).destroy!
+      end
+        event = Event.find(participation_params[:event_id])
+        flash[:success] = "You are now a participant of #{event.name}!"
+        redirect_to event_path(event)
+    rescue
+      flash[:danger] = "There was an error."
+      redirect_to invites_user_path(current_user) 
+    end
+  end
+
+  private
+
+  def participation_params
+    params.permit(:event_id, :user_id)
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user.id.to_s == params[:user_id]
+  end
+
+  def correct_event_invite
+    redirect_to root_url unless EventInvite.exists?(event_id: params[:event_id], invitee_id: params[:user_id])
+  end
+end
--- test/integration/respond_to_event_invites_test.rb
+++ test/integration/respond_to_event_invites_test.rb
@@ -3,9 +3,10 @@ require "test_helper"
 class RespondToEventInvitesTest < ActionDispatch::IntegrationTest
   def setup
     @alice = users(:alice)
-    @event = events(:one)
     @bob = users(:bob)
     @invite_to_alice = event_invites(:one)
+    @event = @invite_to_alice.event
+    @user_with_no_invites = users(:greg)
   end
 
   test "must be logged in to view event invites" do
@@ -30,9 +31,44 @@ class RespondToEventInvitesTest < ActionDispatch::IntegrationTest
     @alice.event_invites.each do |invite|
       assert_select 'li.event-invite', text: Regexp.new(invite.event.name)
       assert_select 'li.event-invite', text: Regexp.new(invite.event.host.name)
-      assert_select 'a[href=?]', participations_path(event: invite.event.id, user: invite.invitee.id), text: /accept/i
+      assert_select 'form[action=?]', participations_path
+      assert_select 'input[value=?]', invite.event.id.to_s
+      assert_select 'input[value=?]', invite.invitee.id.to_s
+      assert_select 'input[type=submit]', value: /accept/i
       assert_select 'a[href=?]', event_invite_path(invite.id), text: /delete/i
     end
+  end
+
+  test "no invites message" do
+    log_in_as @user_with_no_invites
+
+    get invites_user_path(@user_with_no_invites)
+    assert_select 'p', /you currently have no invitations/i
+  end
+
+  test 'must be logged in as correct user to accept an event invite' do
+    assert_no_difference 'Participation.count' do
+    post participations_path, params: { event_id: @invite_to_alice.event.id, 
+                                        user_id: @alice.id }
+
+    end
+    assert_redirected_to login_url
+
+    log_in_as @bob
+    assert_no_difference 'Participation.count' do
+    post participations_path, params: { event_id: @invite_to_alice.event.id, 
+                                        user_id: @alice.id }
+    end
+    assert_redirected_to root_url
+  end
+
+  test 'can accept an event invite' do
+    log_in_as @alice
+    assert_difference  'Participation.count' => 1, 'EventInvite.count' => -1  do
+      post participations_path, params: { event_id: @invite_to_alice.event.id, 
+                                          user_id: @alice.id }
+    end
+    assert_redirected_to @event
   end
 
   test "must be logged in as correct user to delete an event invite"do
