d949852c43c985a57f6a57a39b4ef7948904e9bd
Brandon Ling <brandonvling@gmail.com>
2021-05-26 21:52:59 -0400

    Add EventInvite validations

--- app/models/event_invite.rb
+++ app/models/event_invite.rb
@@ -1,4 +1,15 @@
 class EventInvite < ApplicationRecord
+  validate :is_already_participant?
   belongs_to :event
   belongs_to :invitee, class_name: "User"
+  validates :status, presence: true
+  validates :invitee, uniqueness: { scope: :event }
+
+  private
+  
+    def is_already_participant?
+      if event.participants.include? invitee
+        errors.add(:invitee, "Invitee is already a participant.")
+      end
+    end
 end
--- test/models/event_invite_test.rb
+++ test/models/event_invite_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class EventInviteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @invitee = users(:eve)
+    @event = events(:one)
+    @invitation = EventInvite.new(event: @event, 
+                                  invitee: @invitee)
+  end
+
+  test "validates event invite" do
+    assert @invitation.valid?
+  end
+
+  test "default event invite status is Pending" do
+    assert @invitation.status = "Pending"
+  end
+
+  test "status is not blank" do
+    @invitation.status = ''
+    assert_not @invitation.valid?
+    @invitation.status = nil
+    assert_not @invitation.valid? 
+  end
+
+  test "only one invite per person per event" do
+    dup_invitation = @invitation.dup
+    assert dup_invitation.valid?
+    @invitation.save
+    dup_invitation.save
+    assert_not dup_invitation.valid?
+  end
+
+  test "can only invite users who aren't already participants" do
+    invite = EventInvite.new(event: @event, invitee: users(:bob))
+    assert_not invite.valid?
+  end
 end
