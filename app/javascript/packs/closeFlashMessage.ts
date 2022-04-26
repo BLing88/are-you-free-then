30731bc1500c245ddf826f3313c011e53fefbf67
Brandon Ling <brandonvling@gmail.com>
2022-04-26 17:59:06 -0400

    Trap focus on close button and center flash message

--- app/javascript/packs/closeFlashMessage.ts
+++ app/javascript/packs/closeFlashMessage.ts
@@ -2,6 +2,13 @@ function closeMessage(el: Element): void {
   el.remove();
 }
 
+function trapFocus(event) {
+  if (event.key === "Tab") {
+    event.preventDefault();
+    event.currentTarget.focus();
+  }
+}
+
 document.addEventListener("turbolinks:load", () => {
   const flashMessageContainer = document.querySelector(
     ".flash-message-container"
@@ -9,9 +16,13 @@ document.addEventListener("turbolinks:load", () => {
 
   if (flashMessageContainer) {
     const closeBtn = document.querySelector(".close-btn");
+    closeBtn.focus();
 
     closeBtn.addEventListener("click", function () {
       closeMessage(flashMessageContainer);
+      closeMessage(flashBackground);
     });
+
+    closeBtn.addEventListener("keydown", trapFocus);
   }
 });
