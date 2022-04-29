cf40a300f134ed329cf097d11a69a9a14050ea7d
Brandon Ling <brandonvling@gmail.com>
2022-04-29 14:18:03 -0400

    Change header menu icon to use button

--- app/javascript/packs/toggleHeaderMenu.ts
+++ app/javascript/packs/toggleHeaderMenu.ts
@@ -2,7 +2,7 @@ document.addEventListener("turbolinks:load", () => {
   const menuContainer: HTMLElement = document.querySelector(".header-menu-li");
   if (menuContainer) {
     const headerMenu: HTMLElement = document.querySelector(".header-menu");
-    const headerMenuIcon = document.querySelector(".header-menu-icon");
+    const headerMenuIcon = document.querySelector(".header-menu-btn");
     headerMenuIcon.addEventListener("click", () => {
       headerMenu.style.visibility =
         headerMenu.style.visibility === "hidden" ? "visible" : "hidden";
